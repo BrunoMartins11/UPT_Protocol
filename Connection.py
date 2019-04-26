d54f67d7b651eb120783dc48284e0ae2f882e0ae
Eduardo Barbosa <herulume@protonmail.com>
2019-04-26 10:13:27 +0000

    :unicorn:

--- Connection.py
+++ Connection.py
@@ -8,28 +8,22 @@ class Connection:
         self.host = host
         self.port = port
         self.max_buf_size = 5
-        self.outfile = open("out_{0}".format(filename), "wb")
+        self.outfile = open("reV_{0}".format(filename), "wb")
         self.seqnums = {}  # enforce single instance of each seqno
 
     def ack(self, seqno, data):
         res_data = []
         self.updated = time.time()
-        # if the sequence number of the received packet is larger than the current sequence number and
-        # the window size is not exceeded
         if (seqno == self.current_seqno) and self.seqnums.__len__() <= self.max_buf_size:
-            # Add the data to the window with the sequence number as the lookup value
             self.seqnums[seqno] = data
-            # Then, for every sequence number
             for n in sorted(self.seqnums.keys()):
-                # If the sequence number is equal to the one we need
                 if n == self.current_seqno:
-                    # "Receive" and rebuild the data and remove if from the window
                     self.current_seqno += len(data)
                     res_data.append(self.seqnums[n])
                     del self.seqnums[n]
-                    break  # when we find out of order seqno, quit and move on
+                    break
 
-        # note: we return the sequence number of the last packet received
+        # return seqno of the last packet received
         return (self.current_seqno - len(data)), res_data
 
     def record(self, data):
