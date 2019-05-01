8b829cdeb0fd1c3b9af8d260a9dc4224160dc4dd
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-01 11:24:15 +0100

    Finish Upt client and server

--- Upt_client.py
+++ Upt_client.py
@@ -1,13 +1,30 @@
 import socket
 from random import randint
+from Receiver import Receiver
+from Sender import Sender
 
 def client(port):
     sock = socket.socket()
     sock.connect(('127.0.0.1', port))
     while True:
         cmd = input("> ")
-        sock.send(str.encode(cmd))
-        if cmd == 'exit':
+        cmd = cmd.split(" ")
+        if cmd[0] == 'get':
+            port = randint(10000,40000)
+            address = "localhost"
+            timeout = 3
+            request = "get {} {} {}".format(address, port, cmd[1])
+            print("Sending: {}".format(request))
+            sock.send(str.encode(request))
+            Receiver(port, timeout).start()
+        elif cmd[0] == 'put':
+            sock.send(str.encode(cmd[0]))
+            response = sock.recv(4096).decode("utf-8").split(" ")
+            print(" ".join(response))
+            Sender(response[1], int(response[2]), cmd[1]).start()
+        else:
+            sock.send(str.encode(" ".join(cmd)))
+        if cmd[0] == 'exit':
             break
         print(sock.recv(4096).decode("utf-8"))
 
--- Upt_server.py
+++ Upt_server.py
@@ -2,13 +2,63 @@ import socket
 from random import randint
 from subprocess import check_output
 import os
+from threading import Thread
+from Sender import Sender
+from Receiver import Receiver
 
 def run_ls(args):
     return check_output(args)
 
+def get_file(args):
+    port = int(args[0])
+    timeout = int(args[1])
+    Receiver(port, timeout).start()
+
+def send_file(args):
+    port = int(args[0])
+    timeout = int(args[1])
+    filename = args[2]
+    Sender(port, timeout, filename).start()
+
 cmds = {
-        "ls": lambda a: run_ls(["ls"] + a),
-        }
+    "ls": lambda a: run_ls(["ls"] + a),
+    "get": lambda a: get_file(a),
+    "put": lambda a: put_file(a)
+    }
+
+class ClientThread(Thread):
+    def __init__(self, socket):
+        Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        while True:
+            data = str(self.socket.recv(4096).decode("utf-8"))
+            print("MESSAGE RECEIVED: {}".format(data))
+            data = data.split(" ")
+            if data[0] == 'exit':
+                break
+            if data[0] == 'ls':
+                out = run_ls(data)
+            elif data[0] == 'get':
+                address = data[1]
+                port = data[2]
+                filename = data[3]
+                print("Sending file: {} {} {}".format(address, port, filename))
+                Sender(address, port, filename).start()
+                out = b'Sent'
+            elif data[0] == 'put':
+                port = randint(10000, 40000)
+                cmd = 'Receiving_on: localhost {}'.format(port)
+                self.socket.send(cmd)
+                print("Receiving file: {}".format(port))
+                Receiver(port, 3).start()
+                out = b'Received'
+            else:
+                out = b'Invalid command'
+            self.socket.send(out)
+        self.socket.close()
+
 
 def server(directory='.'):
     sock = socket.socket()
@@ -27,15 +77,5 @@ def server(directory='.'):
     sock.listen(10)
     while True:
         conn, addr = sock.accept()
-        while True:
-            data = str(conn.recv(4096).decode("utf-8"))
-            data = data.split(" ")
-            if data[0] == 'exit':
-                break
-            if data[0] in cmds:
-                out = cmds[data[0]](data[1:])
-            else:
-                out = b'Invalid command'
-            conn.send(out)
-        conn.close()
+        ClientThread(conn).start()
 
