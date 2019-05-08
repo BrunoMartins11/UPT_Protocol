0eee973e2fd5cc6a3de6ba5c258e4e313d3b9f5c
Eduardo Barbosa <herulume@protonmail.com>
2019-05-08 22:59:16 +0000

    m

--- Sender.py
+++ Sender.py
@@ -2,24 +2,36 @@ import os
 
 import socket
 
+from math import ceil
 import random
 import argparse
 
 import Packet
 from Window import Window
 
+import utils as consts
+
 
 class Sender:
-    def __init__(self, dest, port, filename, timeout_t=10, attempts_n=5):
+    def __init__(self, dest, port, filename, timeout_t=10):
         self.current_state = 0
+        self.rtimeout = timeout_t
+        self.attempts = consts.RESEND_MAX
+        self.rwindow = 0
+
+        self.window = Window(10)  # [[seqno, data, sent]]
+
         self.dest = dest
         self.dport = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.settimeout(None)
         self.sock.bind(('', random.randint(10000, 40000)))
-        self.window = Window(10, filename)  # [[seqno, data, sent]]
-        self.rtimeout = timeout_t
-        self.attempts = attempts_n
+
+        self.infile = open(filename, "rb")
+        self.filesize = 0
+        self.filename = filename
+        self.total_packets = ceil(os.stat(filename).st_size / consts.DATA_SIZE)
+
         self.MESSAGE_HANDLER = {
             'ack': self.handle_ack
         }
@@ -36,8 +48,42 @@ class Sender:
             address = (self.dest, self.dport)
         self.sock.sendto(message, address)
 
+    def load_file(self):
+        # seqno += number of bytes in the current packet
+
+        self.window.push([self.window.initial_sn, self.filename.encode('utf-8'), False])
+        self.window.initial_sn += len(self.filename.encode('utf-8'))
+        self.window.current_sn = self.window.initial_sn
+
+        with open(self.filename, 'rb') as sending_file:
+            self.filesize = os.stat(self.filename).st_size
+
+            while (self.window.can_send(self.rwindow)) and (
+                    self.filesize > (self.window.current_sn - self.window.initial_sn)):
+                sending_file.seek(self.window.current_sn - self.window.initial_sn)
+                next_packet = sending_file.read(consts.DATA_SIZE)
+                self.window.push([self.window.current_sn, next_packet, False])
+                self.window.current_sn += len(next_packet)
+
+            if self.window.can_send(self.rwindow):
+                self.window.push([self.window.current_sn, b'', False])  # 'end' packet
+
+    def update_sliding_window(self):
+        with open(self.filename, 'rb') as sending_file:
+            while (self.window.can_send(self.rwindow) and
+                   (self.filesize > (self.window.current_sn - self.window.initial_sn))):
+                sending_file.seek(self.window.current_sn - self.window.initial_sn)
+                next_packet = sending_file.read(consts.DATA_SIZE)
+                self.window.push([self.window.current_sn, next_packet, False])
+                self.window.current_sn += len(next_packet)
+
+        if self.current_state == 0:
+            self.increment_state()
+        if len(self.window.msg_window) <= 1:
+            self.increment_state()
+
     def start(self):
-        self.window.load_file()
+        self.load_file()
 
         # 0: Transfer not started
         # 1: Transfer in progress
@@ -51,7 +97,7 @@ class Sender:
                               (self.dest, self.dport))
                     self.window.msg_window[0][2] = True
                 elif self.current_state == 1:
-                    self.send_next_data()
+                    self.send_data()
                 elif self.current_state == 2:
                     self.send(Packet.make_packet('end', self.window.msg_window[0][0], self.window.msg_window[0][1]),
                               (self.dest, self.dport))
@@ -66,9 +112,7 @@ class Sender:
                     if Packet.validate_checksum(message):
                         self.MESSAGE_HANDLER.get(msg_type, self._handle_other)(seqno, data)
                 else:
-                    if self.current_state != 0:
-                        self.resend_data()
-                        self.attempts -= 1
+                    self.attempts -= 1
 
                 if self.attempts <= 0:
                     raise TimeoutError
@@ -78,38 +122,23 @@ class Sender:
     def increment_state(self):
         self.current_state += 1
 
-    def resend_data(self):
-        try:
-            i = 0
-            while i < len(self.msg_window):
-                if self.msg_window[i][2]:
-                    self.send(Packet.make_packet('data', self.msg_window[i][0], self.msg_window[i][1]),
-                              (self.dest, self.dport))
-                    self.msg_window[i][2] = True
-                i += 1
-        except:
-            pass
+    def send_data(self):
+        seqs = []
+        if self.window.action == consts.TRANS:
+            seqs = self.window.to_send()
+        elif self.window.action == consts.RETRANS:
+            seqs = self.window.to_ack()
 
-    def send_next_data(self):
-        if len(self.msg_window) > 0:
-            packet_to_send = False
-            i = 0
-            while (not packet_to_send) and (i < len(self.msg_window)):
-                if not self.msg_window[i][2]:
-                    packet_to_send = True
-                else:
-                    i += 1
-
-            if packet_to_send:
-                while i < len(self.msg_window):
-                    self.send(Packet.make_packet('data', self.msg_window[i][0], self.msg_window[i][1]),
-                              (self.dest, self.dport))
-                    self.msg_window[i][2] = True
-                    i += 1
-        pass
+        for (msg, i) in seqs:
+            self.send(Packet.make_packet('data', self.window.msg_window[i][0], self.window.msg_window[i][1]),
+                      (self.dest, self.dport))
+            self.window.msg_window[i][2] = True
 
     def handle_ack(self, seqno, data):
-        self.window.ack(seqno, data)
+        self.rwindow = int(data)
+        update = self.window.ack(seqno)
+        if update:
+            self.update_sliding_window()
 
     def _handle_other(self, seqno, data):
         pass
--- Window.py
+++ Window.py
@@ -1,27 +1,24 @@
-import os
 import utils as CC
 from random import randint
 
 
 class Window:
 
-    def __init__(self, ws, filename):
-        self.rwindow = 0
+    def __init__(self, ws):
         self.msg_window = []
         self.initial_sn = randint(0, 65535)
         self.current_sn = self.initial_sn
-        self.infile = open(filename, "rb")
-        self.filesize = 0
-        self.filename = filename
         self.ws = ws
         self.cwnd = 1
-        self.dupack = 0
+        self.dup_ack = 0
         self.ssthresh = 64
         self.state = CC.SLOW_START
         self.action = CC.TRANS
 
-    def ack(self, seqno, data):
-        self.rwindow = int(data)
+    def push(self, data):
+        self.msg_window.append(data)
+
+    def ack(self, seqno):
         if self.state == CC.SLOW_START:
             self.slow_start(seqno)
         elif self.state == CC.AVOID_CONGEST:
@@ -42,39 +39,41 @@ class Window:
         if len(temp_packet) > 0:
             if self.msg_window[temp_index][2]:
                 del self.msg_window[temp_index]
-                self.update_sliding_window()
+                return True
+        return False
 
-    def slow_start(self, acknum):
+    def slow_start(self, seqno):
         if self.cwnd >= self.ssthresh:
             self.state = CC.AVOID_CONGEST
-            self.avoid(acknum)
-        elif acknum == self.base:
+            self.avoid(seqno)
+        elif list(filter(lambda x: x[2] and x[0] == seqno, self.msg_window)):
             self.cwnd += 1
-            self.dupack = 0
+            self.dup_ack = 0
             self.action = CC.TRANS
         else:
-            self.dupack += 1
-            if self.dupack == 3:
+            self.dup_ack += 1
+            if self.dup_ack == 3:
                 self.to_quick_r()
 
-    def avoid(self, acknum):
-        if acknum == self.base:
+    def avoid(self, seqno):
+        if list(filter(lambda x: x[2] and x[0] == seqno, self.msg_window)):
             self.cwnd += (1 / self.cwnd)
-            self.dupack = 0
+            self.dup_ack = 0
             self.action = CC.TRANS
         else:
-            self.dupack += 1
-            if self.dupack == 3:
+            self.dup_ack += 1
+            if self.dup_ack == 3:
                 self.to_quick_r()
 
-    def quick_r(self, acknum):
-        if acknum < self.base:
+    def quick_r(self, seqno):
+        old_ack = list(filter(lambda x: x[2] and x[0] == seqno, self.msg_window))
+        if not old_ack:
             self.cwnd += 1
             self.action = CC.TRANS
-        elif acknum == self.base:
+        elif old_ack:
             self.state = CC.AVOID_CONGEST
             self.cwnd = self.ssthresh
-            self.dupack = 0
+            self.dup_ack = 0
 
     def to_quick_r(self):
         self.state = CC.QUICK_RECOVER
@@ -82,43 +81,28 @@ class Window:
         self.ssthresh = self.cwnd / 2
         self.cwnd = self.ssthresh + 3
 
-    def update_sliding_window(self):
-        with open(self.filename, 'rb') as sending_file:
-            if len(self.msg_window) < 5:
-                while ((len(self.msg_window) < 5) and
-                       (self.filesize > (self.current_sn - self.initial_sn))):
-                    sending_file.seek(self.current_sn - self.initial_sn)
-                    next_packet = sending_file.read(1458)
-                    self.msg_window.append([self.current_sn, next_packet, False])
-                    self.current_sn += len(next_packet)
-
     def can_send(self, rwnd):
         return len(self.msg_window) < min([self.cwnd, self.ws, rwnd])
-
-    def load_file(self):
-        # seqno += number of bytes in the current packet
-
-        self.msg_window.append([self.current_sn, self.filename.encode('utf-8'), False])
-        self.initial_sn += len(self.filename.encode('utf-8'))
-        self.current_sn = self.initial_sn
-
-        with open(self.filename, 'rb') as sending_file:
-            self.filesize = os.stat(self.filename).st_size
-
-            while (len(self.msg_window) < 5) and (self.filesize > (self.current_sn - self.initial_sn)):
-                sending_file.seek(self.current_sn - self.initial_sn)
-                next_packet = sending_file.read(1458)
-                self.msg_window.append([self.current_sn, next_packet, False])
-                self.current_sn += len(next_packet)
-
-            if len(self.msg_window) < 5:
-                self.msg_window.append([self.current_sn, b'', False])  # 'end' packet
-
-    # MERDAS
 
     def timeout(self):
         self.ssthresh = self.cwnd / 2
         self.cwnd = 1
-        self.dupack = 0
+        self.dup_ack = 0
         self.state = CC.SLOW_START
         self.action = CC.RETRANS
+
+    def to_send(self):
+        packets = []
+        i = 0
+        while i < len(self.msg_window):
+            if not self.msg_window[i][2]:
+                packets.append((self.msg_window[i], i))
+            i += 1
+
+    def to_ack(self):
+        packets = []
+        i = 0
+        while i < len(self.msg_window):
+            if self.msg_window[i][2]:
+                packets.append((self.msg_window[i], i))
+            i += 1
